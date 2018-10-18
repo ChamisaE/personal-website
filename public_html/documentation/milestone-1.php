79a24f4f7232af87cb2801a8b2ce1eb4a92d5021
ChamisaE <43251409+ChamisaE@users.noreply.github.com>
2018-10-17 21:44:04 -0600

    html markup with audience statement. Includes some basic text formatting.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,8 +6,13 @@
 	</head>
 	<body>
 		<h1><strong>Purpose</strong></h1>
-		<p>The mission of this company is to be one of the foremost providers of technological expertise
-		education in Indian Country. Using portfolios of previous projects we have developed and contributed
-		to, we seek to develop innovative solutions for our clients needs.</p>
+		<p align="left">The mission of this company is to be one of the foremost providers of technological expertise
+		education<br/>in Indian Country. Using portfolios of previous projects we have developed and contributed
+		to, we seek to<br/> develop innovative solutions for our clients needs.</p>
+		<h1><strong>Audience</strong></h1>
+		<p align="left">This website will be used to attract future clients for to my company.<br/>It will have a simple design
+		with a sleek, simple and smooth navigation process.<br/>The website's audience will be made up of people from
+		a variety of backgrounds and comfort levels<br/>ranging from young students and their teachers to tech
+			professionals and prospective employees. </p>
 	</body>
 </html>
