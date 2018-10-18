dfc889fa4f6ea3fde6e6d05943805c9bebfc41df
Chamisa Edmo <chamisaedmo@yahoo.com>
2018-10-17 23:12:01 -0600

    Edited errors, cleared through html check. Full text milestone 1.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -25,8 +25,8 @@
 			when I start to build my company. It will give me the opportunity to continue to hone my<br/>
 			skills as a developer, and serve as a skeleton for me on future websites.</p>
 		<h1><strong>Persona</strong></h1>
-		<img src ="nusindahYaz.jpg">
-		<p><strong>Nusindah Yazzie</strong></p>
+		<img src ="nusindahYaz.jpg" alt="Nusindah Yazzie">
+		<h3>Nusindah Yazzie</h3>
 		<ul>
 			<li>40 yo</li>
 			<li>Married</li>
@@ -34,11 +34,11 @@
 			<li>M.Ed.</li>
 			<li>High school science teacher, engineering club sponsor</li>
 		</ul>
+		<h4>Profession</h4>
 		<p>Nusindah is an educational enthusiast who is constantly looking for ways to bring fresh<br/>
 		and enriching activities to her classroom. One of her main goals is to connect Native youth<br/>
-		with Native professionals working in STEAM fields.<br/><br/>
-			Relevant Technologies:
-		</p>
+		with Native professionals working in STEAM fields.<br/><br/></p>
+			<h4>Relevant Technologies:</h4>
 		<ul>
 			<li>iPhone 8</li>
 			<li>Personal Macbook Pro (silver, 13-inch LED-backlit display with IPS technology<br/>
@@ -49,13 +49,17 @@
 				Intel Celeron 2955U 1.4 GHz, 11.6-in anti-glare screen, HDMI port<br/>
 				(the ones that have been discontinued)</li>
 		</ul>
+		<h4>Attitudes and Behaviors</h4>
 		<p>Nusindah is bilingual (Navajo and English). She is comfortable using computers<br/>
 			and can confidently navigate and use basic classroom technologies (projectors, laptops, etc.).<br/>
 			She has bought some basic engineering kits for her students, but has not appreciated the lack of<br/>
 			technical support from the companies that provide them. She is a regular internet user<br/>
-		and often spends an additional 15-20 hours outside of work preparing activities and projects she's ordered.</p>
+		and spends an additional 15-20 frustrated hours outside of work preparing activities and projects she's ordered.</p>
+		<h4>Goals</h4>
 		<p>This user will visit my site to connect about educational kits we provide. She wants to explore what we<br/>
 		offer for order online and how she can implement those activities in her club and classroom. She is excited<br/>
 		to find my company because it is both Native-owned and exactly what she has been looking.</p>
+		<h4>Use Case</h4>
+		<p>Nusindah needs to quickly assess what this company has to offer and contact them on her lunch break.</p>
 	</body>
 </html>
