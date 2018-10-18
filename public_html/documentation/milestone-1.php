a4f158abd14201a6ab2985d02e16dcf5eed4dca7
ChamisaE <43251409+ChamisaE@users.noreply.github.com>
2018-10-17 21:33:08 -0600

    basic html markup with mission statement.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Chamisa Edmo</title>
+	</head>
+	<body>
+		<h1><strong>Purpose</strong></h1>
+		<p>The mission of this company is to be one of the foremost providers of technological expertise
+		education in Indian Country. Using portfolios of previous projects we have developed and contributed
+		to, we seek to develop innovative solutions for our clients needs.</p>
+	</body>
+</html>
