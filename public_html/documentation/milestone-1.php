3f88b884551c8923856be5e1f4720789ccb6403e
ChamisaE <43251409+ChamisaE@users.noreply.github.com>
2018-10-17 22:06:03 -0600

    markup with purpose, audience and goals. Cleared through HTML checker.

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -6,13 +6,23 @@
 	</head>
 	<body>
 		<h1><strong>Purpose</strong></h1>
-		<p align="left">The mission of this company is to be one of the foremost providers of technological expertise
+		<p>The mission of this company is to be one of the foremost providers of technological expertise
 		education<br/>in Indian Country. Using portfolios of previous projects we have developed and contributed
 		to, we seek to<br/> develop innovative solutions for our clients needs.</p>
 		<h1><strong>Audience</strong></h1>
-		<p align="left">This website will be used to attract future clients for to my company.<br/>It will have a simple design
+		<p>This website will be used to attract future clients for to my company.<br/>It will have a simple design
 		with a sleek, simple and smooth navigation process.<br/>The website's audience will be made up of people from
 		a variety of backgrounds and comfort levels<br/>ranging from young students and their teachers to tech
 			professionals and prospective employees. </p>
+		<h1><strong>Goal</strong></h1>
+		<p>Initially I will be using this site to: </p>
+		<ul>
+			<li>as a place to present the projects I contributed to and work I have done.</li>
+			<li>highlight the skills I aquired in the Deep Dive Bootcamp.</li>
+			<li>get a web/app development internship after I graduate.</li>
+		</ul>
+		<p>Once I get an internship, I will use this page as an example of work I am capable of creating<br/>
+			when I start to build my company. It will give me the opportunity to continue to hone my<br/>
+			skills as a developer, and serve as a skeleton for me on future websites.</p>
 	</body>
 </html>
