90b9dbe556582683dbd1781566da99a6aae55369
Chamisa Edmo <chamisaedmo@yahoo.com>
2018-10-31 16:57:40 -0600

    fixed anchor tag for pdf link again

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,7 +5,7 @@
 		<title>Chamisa Edmo</title>
 	</head>
 	<body>
-		<a href = "PWPwireframes.pdf" alt="2a Wireframes"></a>
+		<a href = "PWPwireframes.pdf">Wireframes</a>
 		<h1><strong>Purpose</strong></h1>
 		<p>The mission of this company is to be one of the foremost providers of technological expertise
 		education<br/>in Indian Country. Using portfolios of previous projects we have developed and contributed
