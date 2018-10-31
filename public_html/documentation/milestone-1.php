c4194efaea28cdae1943d2a3176838c17e3fb2c5
Chamisa Edmo <chamisaedmo@yahoo.com>
2018-10-31 16:54:29 -0600

    Added milestone 2 wireframes as pdf with link

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -5,6 +5,7 @@
 		<title>Chamisa Edmo</title>
 	</head>
 	<body>
+		<img src="PWPwireframes.pdf" alt="2a Wireframes">
 		<h1><strong>Purpose</strong></h1>
 		<p>The mission of this company is to be one of the foremost providers of technological expertise
 		education<br/>in Indian Country. Using portfolios of previous projects we have developed and contributed
